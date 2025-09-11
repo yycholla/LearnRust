cf65d8f40c18aada0c01259b441f691fd9186f22
chanway <chanway@hey.com>
2025-09-11 15:41:18 -0600

    update to current

--- lists/src/lib.rs
+++ lists/src/lib.rs
@@ -1,17 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
+pub mod fifth;
 pub mod first;
+pub mod fourth;
 pub mod second;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod third;
